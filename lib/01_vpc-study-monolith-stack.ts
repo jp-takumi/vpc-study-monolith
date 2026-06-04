de74a2e3a6a0553ea8f3e2ce41b5fd5fb9f2e6fd
jp-takumi <taku2003mi@outlook.jp>
2026-06-04 20:35:53 +0900

    いったんVPC作成おわり次lambda使ってデフォルトのrtの名前つけたい

--- lib/01_vpc-study-monolith-stack.ts
+++ lib/01_vpc-study-monolith-stack.ts
@@ -5,10 +5,9 @@
 
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// ※今後、ここに ec2 などを密結合で追加していきます！
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ec2 from 'aws-cdk-lib/aws-ec2'; 
+import * as cr from 'aws-cdk-lib/custom-resources';
 
-// 💡 先頭の「01」を消して、アルファベットの「V」から始めます！
 export class VpcStudyMonolithStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -25,5 +24,31 @@ export class VpcStudyMonolithStack extends cdk.Stack {
       tags: [{ key: 'Name', value: 'my-monolith-vpc'}],
     });
 
+    //--------------------------------- 
+    // subnetを作る
+    //---------------------------------
+    const publicSubnet = new ec2.CfnSubnet(this, 'SimpleSubnet',{
+      vpcId: vpc.ref,
+      cidrBlock: '10.0.1.0/24',
+      availabilityZone: 'ap-northeast-1a',
+      mapPublicIpOnLaunch: true,
+      tags: [{ key: 'Name', value: 'MyPublicSubnet'}],
+    });
+
+    //--------------------------------- 
+    // routetableを作成
+    //---------------------------------
+    const routeTable = new ec2.CfnRouteTable(this, 'MyRouteTable',{
+      vpcId: vpc.ref,
+      tags: [{ key: 'Name', value: 'my-monolith-rt'}],
+    });
+
+    //--------------------------------- 
+    // subnetとroutetableを紐づけ
+    //---------------------------------
+    new ec2.CfnSubnetRouteTableAssociation(this, "MySubnetAssociation",{
+      subnetId: publicSubnet.ref,
+      routeTableId: routeTable.ref,
+    });
   }
 }
