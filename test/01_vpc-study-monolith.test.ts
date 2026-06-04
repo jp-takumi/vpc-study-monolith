f2956a1daeec4a3d088d3b082ca5588bfcfa5558
jp-takumi <taku2003mi@outlook.jp>
2026-06-04 19:44:22 +0900

    Initial commit

---
+++ test/01_vpc-study-monolith.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as 01VpcStudyMonolith from '../lib/01_vpc-study-monolith-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/01_vpc-study-monolith-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new 01VpcStudyMonolith.01VpcStudyMonolithStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
